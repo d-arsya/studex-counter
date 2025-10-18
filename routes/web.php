8f04a63f491e98db02ea3872eb0f6f89fb543b49
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-10-18 12:27:54 +0700

    feat: Phone and message

--- routes/web.php
+++ routes/web.php
@@ -46,35 +46,35 @@ Route::post('webhook', function (Request $request) {
     }
 })->withoutMiddleware(VerifyCsrfToken::class);
 
-Route::get('coba', function () {
-    $data = DB::table('datas')->latest()->limit(100)->pluck('webhookdata');
-    $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
-    $sw = [];
-    foreach ($data as $value) {
-        $da = json_decode($value);
-        if (in_array($da->from, $groups)) {
-            $exist = Contact::where('identifier', $da->author)->first();
-            if (!$exist) {
-                Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
-            }
-            $sw[] = $da;
-            $contact = Contact::where('identifier', $da->author)->first()->id;
-            $group = Group::where('identifier', $da->from)->first()->id;
-            $exist = Message::where('identifier', $da->id->id)->first();
-            if (!$exist) {
-                Message::create([
-                    'group_id' => $group,
-                    'contact_id' => $contact,
-                    'text' => $da->body,
-                    'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
-                    'identifier' => $da->id->id
-                ]);
-            }
-            if ($da->hasQuotedMsg) {
-                Message::where('identifier', $da->_data->quotedStanzaID)->update(['replied_by' => $da->id->id]);
-            }
-        }
-    }
+// Route::get('coba', function () {
+//     $data = DB::table('datas')->latest()->limit(100)->pluck('webhookdata');
+//     $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
+//     $sw = [];
+//     foreach ($data as $value) {
+//         $da = json_decode($value);
+//         if (in_array($da->from, $groups)) {
+//             $exist = Contact::where('identifier', $da->author)->first();
+//             if (!$exist) {
+//                 Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
+//             }
+//             $sw[] = $da;
+//             $contact = Contact::where('identifier', $da->author)->first()->id;
+//             $group = Group::where('identifier', $da->from)->first()->id;
+//             $exist = Message::where('identifier', $da->id->id)->first();
+//             if (!$exist) {
+//                 Message::create([
+//                     'group_id' => $group,
+//                     'contact_id' => $contact,
+//                     'text' => $da->body,
+//                     'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
+//                     'identifier' => $da->id->id
+//                 ]);
+//             }
+//             if ($da->hasQuotedMsg) {
+//                 Message::where('identifier', $da->_data->quotedStanzaID)->update(['replied_by' => $da->id->id]);
+//             }
+//         }
+//     }
 
-    return response()->json($sw);
-});
+//     return response()->json($sw);
+// });
