46c4d1d1af2d68122460cc4450af05f585c85957
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-10-18 13:10:55 +0700

    feat: import after insert webhook

---
+++ app/Console/Commands/DecodeData.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Contact;
+use App\Models\Group;
+use App\Models\Message;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DecodeData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'decode:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $data = DB::table('datas')->latest()->pluck('webhookdata');
+        $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
+        $sw = [];
+        foreach ($data as $value) {
+            $da = json_decode($value);
+            if (in_array($da->from, $groups)) {
+                $exist = Contact::where('identifier', $da->author)->first();
+                if (!$exist) {
+                    Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
+                }
+                $sw[] = $da;
+                $contact = Contact::where('identifier', $da->author)->first()->id;
+                $group = Group::where('identifier', $da->from)->first()->id;
+                $exist = Message::where('identifier', $da->id->id)->first();
+                if (!$exist) {
+                    Message::create([
+                        'group_id' => $group,
+                        'contact_id' => $contact,
+                        'text' => $da->body,
+                        'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
+                        'identifier' => $da->id->id
+                    ]);
+                }
+                if ($da->hasQuotedMsg) {
+                    Message::where('identifier', $da->_data->quotedStanzaID)->update(['replied_by' => $da->id->id]);
+                }
+            }
+        }
+        DB::table('datas')->delete();
+        return 0;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Models\Contact;
-use App\Models\Group;
-use App\Models\Message;
-use Carbon\Carbon;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -16,73 +13,9 @@ Route::post('webhook', function (Request $request) {
     $data = $request->data;
     try {
         DB::table('datas')->insert(['webhookdata' => json_encode($data)]);
-        // $da = json_decode($inserted->webhookdata);
-        // $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
-        // logs()->info($da->from);
-        // logs()->info($da->body);
-        // if (in_array($da->from, $groups)) {
-        //     $exist = Contact::where('identifier', $da->author)->first();
-        //     if (!$exist) {
-        //         $exist = Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
-        //     }
-        //     logs()->info('Pengirim:' . $exist->name);
-        //     $sw[] = $da;
-        //     $contact = Contact::where('identifier', $da->author)->first()->id;
-        //     $group = Group::where('identifier', $da->from)->first()->id;
-        //     $exist = Message::where('identifier', $da->id->id)->first();
-        //     logs()->info('Message ID:' . $da->id->id);
-        //     if (!$exist) {
-        //         Message::create([
-        //             'group_id' => $group,
-        //             'contact_id' => $contact,
-        //             'text' => $da->body,
-        //             'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
-        //             'identifier' => $da->id->id
-        //         ]);
-        //     }
-        //     if ($da->hasQuotedMsg) {
-        //         Message::where('identifier', $da->_data->quotedStanzaID)->update(['replied_by' => $da->id->id]);
-        //     }
-        //     $inserted->delete();
-        // } else {
-        //     logs()->info('GAGAL');
-        //     logs()->info($da->from);
-        // }
+        Artisan::call('decode:data');
         return response()->json(["success" => true]);
     } catch (\Throwable $th) {
         return response()->json(["success" => false, "message" => $th->getMessage()]);
     }
 })->withoutMiddleware(VerifyCsrfToken::class);
-
-// Route::get('coba', function () {
-//     $data = DB::table('datas')->latest()->limit(100)->pluck('webhookdata');
-//     $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
-//     $sw = [];
-//     foreach ($data as $value) {
-//         $da = json_decode($value);
-//         if (in_array($da->from, $groups)) {
-//             $exist = Contact::where('identifier', $da->author)->first();
-//             if (!$exist) {
-//                 Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
-//             }
-//             $sw[] = $da;
-//             $contact = Contact::where('identifier', $da->author)->first()->id;
-//             $group = Group::where('identifier', $da->from)->first()->id;
-//             $exist = Message::where('identifier', $da->id->id)->first();
-//             if (!$exist) {
-//                 Message::create([
-//                     'group_id' => $group,
-//                     'contact_id' => $contact,
-//                     'text' => $da->body,
-//                     'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
-//                     'identifier' => $da->id->id
-//                 ]);
-//             }
-//             if ($da->hasQuotedMsg) {
-//                 Message::where('identifier', $da->_data->quotedStanzaID)->update(['replied_by' => $da->id->id]);
-//             }
-//         }
-//     }
-
-//     return response()->json($sw);
-// });
