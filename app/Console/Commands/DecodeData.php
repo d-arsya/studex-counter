2d363f31d7dbed19f2471775cbedfe447f62f579
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-10-23 18:26:12 +0700

    da

--- app/Console/Commands/DecodeData.php
+++ app/Console/Commands/DecodeData.php
@@ -33,7 +33,6 @@ class DecodeData extends Command
         $data = DB::table('datas')->latest()->pluck('webhookdata');
         $groups = Group::where('is_cust', true)->pluck('identifier')->toArray();
         $success = true;
-        $sw = [];
         foreach ($data as $value) {
             $da = json_decode($value);
             if (in_array($da->from, $groups)) {
@@ -41,7 +40,6 @@ class DecodeData extends Command
                 if (!$exist) {
                     Contact::create(['name' => $da->_data->notifyName, 'identifier' => $da->author]);
                 }
-                $sw[] = $da;
                 $contact = Contact::where('identifier', $da->author)->first()->id;
                 $group = Group::where('identifier', $da->from)->first()->id;
                 $exist = Message::where('identifier', $da->id->id)->first();
@@ -58,6 +56,9 @@ class DecodeData extends Command
                     }
                     if ($da->hasQuotedMsg) {
                         $quoted = Message::where('identifier', $da->_data->quotedStanzaID)->first();
+                        if ($quoted) {
+                            $quoted = $this->createMessage($da);
+                        }
                         if ($quoted->replied_by == null && strlen($da->body) < 6 && !str_contains(strtolower($da->body), 'm')) {
                             $quoted->update(['replied_by' => $da->id->id]);
                         }
@@ -76,5 +77,21 @@ class DecodeData extends Command
             DB::table('datas')->delete();
         }
         return 0;
+    }
+
+    private function createMessage($da)
+    {
+        $contact = Contact::where('identifier', $da->_data->quotedParticipant)->first()->id;
+
+        $group = Group::where('identifier', $da->from)->first()->id;
+        $data = Message::create([
+            'group_id' => $group,
+            'contact_id' => $contact,
+            'text' => $da->body,
+            'time' => Carbon::createFromTimestamp($da->timestamp)->addHours(7),
+            'identifier' => $da->id->id,
+            'is_delivery' => str_contains(strtolower($da->body), 'jastip')
+        ]);
+        return $data;
     }
 }
